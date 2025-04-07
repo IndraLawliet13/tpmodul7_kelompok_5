65611be60856bfa8aebf959bb6b39b6cf8c8048a
aprilHardinata <aprilhardihardi439@gmail.com>
2025-04-07 14:06:54 +0700

    menambahkan class KuliahMahasiswa103022300027

--- tpmodul7_kelompok_5/Program.cs
+++ tpmodul7_kelompok_5/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// DataMahasiswa
 DataMahasiswa103022300027 dataHandler = new DataMahasiswa103022300027();
 dataHandler.ReadJSON();
+// KuliahMahasiswa
+KuliahMahasiswa103022300027 kuliahHandler = new KuliahMahasiswa103022300027();
+kuliahHandler.ReadJSON();
+Console.WriteLine("\nTekan Enter untuk keluar...");
