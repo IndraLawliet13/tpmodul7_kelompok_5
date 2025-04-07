0be2dfd637bea096d3a06e4a7fa44b2f9de6e800
Indra Yuda <indralawliet13@gmail.com>
2025-04-07 10:29:25 +0700

    menambahkan class KuliahMahasiswa103022300017

--- tpmodul7_kelompok_5/Program.cs
+++ tpmodul7_kelompok_5/Program.cs
@@ -1,4 +1,6 @@
 ﻿DataMahasiswa103022300017 dataHandler = new DataMahasiswa103022300017();
 dataHandler.ReadJSON();
+Console.WriteLine("\n=========================\n");
+KuliahMahasiswa103022300017 kuliahHandler = new KuliahMahasiswa103022300017();
+kuliahHandler.ReadJSON();
 Console.WriteLine("\nTekan Enter untuk keluar...");
-Console.ReadLine();
