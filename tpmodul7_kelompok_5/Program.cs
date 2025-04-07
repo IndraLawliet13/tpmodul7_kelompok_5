3264bd96152c4d66df755b961d63b61ea12b6b94
maxswin99 <andreaschristianfirga06@gmail.com>
2025-04-07 14:05:23 +0700

    menambahkan class kuliah mahasiswa

--- tpmodul7_kelompok_5/Program.cs
+++ tpmodul7_kelompok_5/Program.cs
@@ -1,3 +1,6 @@
 ﻿DataMahasiswa103022300002 dataHandler = new DataMahasiswa103022300002();
 dataHandler.ReadJSON();
 Console.WriteLine("\n=========================\n");
+KuliahMahasiswa103022300002 kuliahHandler = new KuliahMahasiswa103022300002();
+kuliahHandler.ReadJSON();
+Console.WriteLine("\nTekan Enter untuk keluar...");
